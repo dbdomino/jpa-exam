a5433a81a831bea7842c78370403bda2f86895c9
dominod <dbdomino@naver.com>
2024-03-23 04:26:17 +0900

    jpa 실행 예제

--- jpa-basic/src/main/java/com/minod/jpa/JpaApplication.java
+++ jpa-basic/src/main/java/com/minod/jpa/JpaApplication.java
@@ -10,6 +10,8 @@ public class JpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaApplication.class, args);
+
+
 	}
 
 }
--- jpa-basic/src/main/java/com/minod/jpa/JpaMain2.java
+++ jpa-basic/src/main/java/com/minod/jpa/JpaMain2.java
@@ -5,34 +5,40 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import lombok.extern.slf4j.Slf4j;
 
-
+@Slf4j
 public class JpaMain2 {
     public static void main(String[] args) {
-        System.out.println("작동중1");
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        System.out.println("작동중2");
         EntityTransaction tx = em.getTransaction(); // 트랜잭션 얻어옴.
+
         tx.begin(); // 트랜잭션 시작
-        // JPA에서 모든 변경 작업은 트랜잭션 안에서 작업 해야 한다.
-        System.out.println("작동중3");
+
+        // JPA에서 모든 변경 작업은 트랜잭션 안에서 작업 해야 한다. 트랜잭션 단위 안에서가 아니라면 실행이 안된다.
         try {
-            System.out.println("작동중40");
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJPA");
-            System.out.println("작동중41");
+
+
+            Member member1 = new Member();
+            member1.setId(1L);
+            member1.setName("HelloA");
+            System.out.println(member1);
+
+            em.persist(member1);
+
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HelloADD");
+//            log.info("hihi hindMember result : {}", findMember.toString());
 
             tx.commit(); // 트랜잭션 종료
         } catch (Exception e) {
             System.out.println("에러");
+            log.info("error occure ", e);
             tx.rollback();
         } finally {
             em.close();
         }
-
-        System.out.println("작동중5");
-
         emf.close();
 
     }
