591944a4ee236bb66f1a245453ded39d47ae0d2f
dominod <dbdomino@naver.com>
2024-03-25 22:04:42 +0900

    실행

--- jpa-basic/src/main/java/com/minod/jpa/JpaApplication.java
+++ jpa-basic/src/main/java/com/minod/jpa/JpaApplication.java
@@ -1,10 +1,8 @@
 package com.minod.jpa;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
 @SpringBootApplication(scanBasePackages = "com.minod.jpa") // 오오... 어플리케이션 스캔 범위도 설정가능.
 public class JpaApplication {
 
