49dac2673633f5b831935294f2d9e5748e0dde15
dominod <dbdomino@naver.com>
2024-03-26 21:22:20 +0900

    Member객체 참조추가

--- jpa-basic/src/main/java/com/minod/jpa/domain/양방향관계/MbrEach.java
+++ jpa-basic/src/main/java/com/minod/jpa/domain/양방향관계/MbrEach.java
@@ -1,7 +1,6 @@
 package com.minod.jpa.domain.양방향관계;
 
 import jakarta.persistence.*;
-import lombok.ToString;
 
 /** 자동으로 생성되는 테이블명은 mbr로 확인 됨.
  * create table public.mbr_persist (
@@ -11,7 +10,6 @@ import lombok.ToString;
  *     )
  */
 @Entity
-@ToString
 public class MbrEach {
     @Id @GeneratedValue
     private Long id;
@@ -23,7 +21,7 @@ public class MbrEach {
     @ManyToOne  // JPA한테 연관 관계를 알려주기 위해서 어노테이션, 사용 누가 Many고 누가 One인지 구분해줘야 함. DB에서 사용되는 기준을 JPA에 알려주기위함.
     // DB에선 Member가 Many, Team이 One 이다. 그래서 @ManyToOne 을 사용함.
     @JoinColumn(name = "TEAM_ID") // 대상 엔티티의 컬럼 이름 알려줌.
-    private TeamEach teamEach;
+    private TeamEach teamEach; // 기본적으로 eitity 객체가 들어가면 어노테이션으로 참조관계 설정 반드시 해줘야 하는구나.
 
     public MbrEach(){}
     public MbrEach(String username) {
