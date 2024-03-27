91f6424d3af21b44a5cf6983b90f79df195a9049
dominod <dbdomino@naver.com>
2024-03-27 14:04:52 +0900

    단방향 1:N 참조

---
+++ jpa-basic/src/main/java/com/minod/jpa/domain/단방향1대N/TeamOne.java
@@ -0,0 +1,43 @@
+package com.minod.jpa.domain.단방향1대N;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class TeamOne {
+    @Id
+    @GeneratedValue
+    @Column(name ="TEAM_ID")
+    private Long id;
+    private String name;
+
+    @OneToMany
+    @JoinColumn(name="TEAM_ID")
+    List<MbrOne> members = new ArrayList<MbrOne>(); // ArrayList로 초기화 해두는게 관례임.
+
+    public List<MbrOne> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<MbrOne> members) {
+        this.members = members;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
