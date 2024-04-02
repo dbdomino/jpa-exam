f13695976dd7c5c5bc9781e392c2b2bb0116ce86
dominod <dbdomino@naver.com>
2024-04-02 23:11:53 +0900

    쿼리에 paging 기능 적용가능

--- jpa-basic/src/test/java/com/minod/jpa/jpql/JpqlPagingTest.java
+++ jpa-basic/src/test/java/com/minod/jpa/jpql/JpqlPagingTest.java
@@ -1,6 +1,9 @@
 package com.minod.jpa.jpql;
 
+import com.minod.jpa.domain.jpql.DayJpql;
+import com.minod.jpa.domain.jpql.DayLightJpql;
 import com.minod.jpa.domain.jpql.MemberJpql;
+import com.minod.jpa.domain.jpql.TeamJpql;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
@@ -46,5 +49,78 @@ public class JpqlPagingTest {
         } finally {
             em.close();
         }
+    }
+
+    @Test
+    @DisplayName("일대다 연관관계가진 엔티티 페이징API 사용시 문제됨")
+    void jpqlPagingTest2() {
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction(); // 트랜잭션 얻어옴.
+        tx.begin(); // 트랜잭션 시작
+
+        try {
+
+            String jpql = "select t from TeamJpql t";
+//            String jpql = "select t from TeamJpql t join fetch t.memberJpqlList";
+            List<TeamJpql> resultList = em.createQuery(jpql, TeamJpql.class)
+                    .setFirstResult(0) // 조회 시작위치는 어디부터?
+                    .setMaxResults(3)  // 조회 최대 몇개까지?
+                    .getResultList();
+            System.out.println("size:"+resultList.size());
+            for (TeamJpql team : resultList) {
+                System.out.println("teamId:" + team.getId()+", Name:"+team.getName());
+                for(MemberJpql memberJpql: team.getMemberJpqlList()) {
+                    System.out.println("   -> memberJpql = "+ memberJpql.getUsername());
+                }
+            }
+
+            tx.commit(); // 트랜잭션 종료
+        } catch (Exception e) {
+            System.out.println("에러");
+            log.info("error occure ", e);
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+    }
+
+    @Test
+    @DisplayName("일대다 연관관계가진 엔티티 페이징API 패치 조인 대신 @BatchSize로 1+N문제 완화")
+    void jpqlPagingTest3() {
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction(); // 트랜잭션 얻어옴.
+        tx.begin(); // 트랜잭션 시작
+
+        try {
+            // TeamJpql에 BatchSize 어노테이션 설정 해줘야 함.
+            String jpql = "select t from TeamJpql t";
+//            String jpql = "select t from TeamJpql t join fetch t.memberJpqlList";
+            List<TeamJpql> resultList = em.createQuery(jpql, TeamJpql.class)
+                    .setFirstResult(0) // 조회 시작위치는 어디부터?
+                    .setMaxResults(3)  // 조회 최대 몇개까지?
+                    .getResultList();
+            System.out.println("size:"+resultList.size());
+            for (TeamJpql team : resultList) {
+                System.out.println("teamId:" + team.getId()+", Name:"+team.getName());
+                for(MemberJpql memberJpql: team.getMemberJpqlList()) {
+                    System.out.println("   -> memberJpql = "+ memberJpql.getUsername());
+                }
+            }
+
+            tx.commit(); // 트랜잭션 종료
+        } catch (Exception e) {
+            System.out.println("에러");
+            log.info("error occure ", e);
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        DayJpql a = new DayJpql();
+        DayLightJpql a2 = new DayLightJpql();
+        DayLightJpql b = (DayLightJpql) a;
+        DayJpql b2 = (DayJpql) a2;
     }
 }
